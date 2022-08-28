c88dc760bcbc119e88fb44d08b74b7e9bed97334
Sergio <sergo.mergo.1998@inbox.ru>
2022-08-28 11:46:58 +0300

    save2

--- Program.cs
+++ Program.cs
@@ -97,11 +97,120 @@ else Console.WriteLine(number + " - нечетное число");*/
 
 // Задача 8
 
-Console.Write("Введите число: ");
+/*Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= number; i++)
 {
     if (i % 2==0)
     {Console.Write(i + " ");}
-} 
+} */
+
+
+//Классная работа, 2 семинар
+
+
+/*int number = new Random().Next(10,100);
+Console.WriteLine("Рандовное число: " + number);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int maximum = firstDigit;
+if (maximum < secondDigit) maximum = secondDigit;
+
+Console.WriteLine ($"Максимальное цифра в числе {number} - это {maximum}" ); // Интерполяция
+// Конкатенация - складывает строчки, интерполяция преобразует содержание в строчку*/
+
+//Классная работа. Задача 1. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+/*int number = new Random().Next(100, 999);
+Console.WriteLine("Рандомное число: " + number);
+
+int firstDigit = number / 100;
+int secondDigit = number % 10;
+int result = firstDigit * 10 + secondDigit;
+
+Console.WriteLine (result);*/
+
+//Классная работа. Задача 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления. 34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+
+/*Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % secondNumber == 0)
+{
+   Console.WriteLine ($"Число {firstNumber} кратно числу {secondNumber}");
+}
+else
+{
+   int result = firstNumber % secondNumber;
+   Console.WriteLine ($"Число {firstNumber} некратно числу {secondNumber} остаток от деления - {result}");
+}*/
+
+//Классная работа. 3 задача. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+/*Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{ Console.WriteLine ($"{number}  -> да"); }
+
+else
+{
+   Console.WriteLine ($"{number}  -> нет");
+}*/
+//1. String
+
+/*string inputNumber = Console.ReadLine(); // "456"
+Console.WriteLine($"{inputNumber[0]}{inputNumber[2]}");*/
+
+// 2 int
+
+/*int number = new Random().Next (100, 1000);
+int result = (number/100) *10 + number %10;
+
+Console.WriteLine($"Исходное число: {number}, результат: {result}");*/
+
+/*Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{ Console.WriteLine ($"{number}  -> да"); }
+
+else
+{
+   Console.WriteLine ($"{number}  -> нет");
+}*/
+
+/*Домашнее задание:
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+/*Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number % 100 / 10;
+
+Console.WriteLine(number1);*/
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Write("Введите число: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+string inputNumberText = Convert.ToString(inputNumber);
+
+if (inputNumberText.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + inputNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
